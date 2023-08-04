3baea1728c4fcc17f7a6284662425c5f0469a81e
Abdur Rahman <rahmanx661@gmail.com>
2023-08-05 02:07:58 +0600

    added ourawesomeservice

--- src/Page/Home/Home/Home.jsx
+++ src/Page/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import OurService from '../OurService/OurService';
 import AbouOurCompany from '../AboutOurCompany/AbouOurCompany';
+import OurAsesomeService from '../../OurAwesomeService/OurAsesomeService';
 
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
             <Banner></Banner>
             <OurService></OurService>
             <AbouOurCompany></AbouOurCompany>
+            <OurAsesomeService></OurAsesomeService>
         </div>
     );
 };
